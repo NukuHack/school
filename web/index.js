cfecaa84c7916f40136a349f5654a7265cdc94fe
BerenjiBotond <nukuhack@gmail.com>
2024-12-03 13:08:37 +0100

    push_push

--- web/index.js
+++ web/index.js
@@ -6,8 +6,8 @@ let Data;
 // XMLHttpRequest létrehozása
 var xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function(){
-    if(this.readyState == 4&& this.status == 200){
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
         var res = JSON.parse(this.responseText)
         Data = res;
         display(res);
@@ -16,44 +16,60 @@ xhttp.onreadystatechange = function(){
     }
 }
 
-xhttp.open('GET','https://jsonplaceholder.typicode.com/users',true);
+xhttp.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
 
 xhttp.send();
 
 
-
-
-
 function display(Data) {
+    let table_help = "<table><tbody>";
+    table_help += "<th>Name</th><th>Username</th><th>E-mail</th><th>Phone</th><th>More</th>";
     for (let i = 0; i < Data.length; i++) {
         const x = Data[i];
-        let kiirh = kiir.innerHTML;
-        kiirh+=`<div id="out_${x.id}" class="out">`;
-        kiirh+=`<p id="out_${x.id}">${x.name}</p>`;
-        kiirh+=`<input type="button" id="open_${x.id}" onclick="Open_Page(${x.id})" value="More about ${x.name}"><br>`;
-        kiirh+=`</div>`;
-        kiir.innerHTML=kiirh;
+        table_help += `<tr>`;
+        table_help += `<td class="out">
+            <p id="out_name_${x.id}">${x.name}</p>
+        </td>`;
+        table_help += `<td class="out">
+            <p id="out_username_${x.id}">${x.username}</p>
+        </td>`;
+        table_help += `<td class="out">
+            <p id="out_email_${x.id}">${x.email}</p>
+        </td>`;
+        table_help += `<td class="out">
+            <p id="out_phone_${x.id}">${x.phone}</p>
+        </td>`;
+        table_help += `<td>
+            <input type="button" id="out_button_${x.id}" onclick="Open_Page(${x.id})" value="More" class="out_button">
+        </td>`;
+        table_help += `</tr>`;
+
     }
+    table_help += "</tbody></table>";
+    kiir.insertAdjacentHTML('beforeend', table_help);
 }
 
+
 function Open_Page(num) {
-    num --;
+    num--;
     // will be called every time the user clicks on a game and will make a separate "webpage" what will only be an overlay on top of everything else
 
-    page.style.visibility="visible";
-    let page_out = page.innerHTML;
-
-    page_out = `<div class="full_page">`;
-    page_out+= `<p class="game_title">${Data[num].name}</p><br>
-                        <div class="small_desc">Email : ${Data[num].email}</div><br>
-                        <p class="review">Phone : ${Data[num].phone}</p>`;
-
-    page_out+= `<input type="button" value="Close this page" onclick="Close_Page()">`;
-    page_out+= `</div>`;
+    page.style.visibility = "visible";
+    let page_out = `<div class="full_page">`;
+    page_out += `
+        <p class="game_title">${Data[num].name}</p>
+        <p>Email : ${Data[num].email}</p>
+        <p>Phone : ${Data[num].phone}</p>
+        <p>City : ${Data[num].address.city}</p>
+        <p>Address : ${Data[num].address.street}</p>
+        <p>Door : ${Data[num].address.suite}</p>
+    `;
+    page_out += `<input type="button" value="Close this page" onclick="Close_Page()">`;
+    page_out += `</div>`;
 
     page.innerHTML=page_out;
 }
+
 function Close_Page() {
-    page.style.visibility="hidden";
-    page.innerHTML = '';
+    page.style.visibility = "hidden";
 }
